98369439647faee2bb12e7a34780ae9328cff786
vinayreddy2006 <pallevinayreddy26@gmail.com>
2025-12-19 12:39:48 +0530

    Updated API URL for production

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // Your Backend URL
+  baseURL: 'https://agrispine-backend.onrender.com/', // Your Backend URL
   headers: {
     'Content-Type': 'application/json',
   },
